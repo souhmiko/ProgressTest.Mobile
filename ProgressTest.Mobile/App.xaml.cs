c57b972153a80ebef065ae3eb853063fbd71b78e
Penny Lau <pennylau715@gmail.com>
2023-12-01 15:57:24 +0800

    Add project files.

---
+++ ProgressTest.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProgressTest.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ProgressTest.Mobile/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ProgressTest.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
